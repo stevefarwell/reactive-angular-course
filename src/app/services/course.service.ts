f10b063b87660483037cd49e44cbba208f8c2582
Steve Farwell <steve@farwelldesign.com>
2022-05-07 06:55:57 -0700

    add course service

---
+++ src/app/services/course.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Course} from '../model/course';
+import {map, shareReplay} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>('/api/courses')
+      .pipe(
+        map((res) => res['payload']),
+        shareReplay()
+      );
+  }
+}
