62edfe440274b6e10d400c438f0f46d546926385
Steve Farwell <steve@farwelldesign.com>
2022-07-09 13:01:17 -0700

    implement message service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-import {Course, sortCoursesBySeqNo} from '../model/course';
-import {Observable} from 'rxjs';
-import {finalize, map} from 'rxjs/operators';
-import {CourseService} from '../services/course.service';
-import {LoadingService} from '../loading/loading.service';
+import { Component, OnInit } from '@angular/core';
+import { Course, sortCoursesBySeqNo } from '../model/course';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { CourseService } from '../services/course.service';
+import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: [ './home.component.css' ]
 })
 export class HomeComponent implements OnInit {
 
@@ -20,12 +21,12 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private courseService: CourseService,
-    private loadingService: LoadingService) {
+    private loadingService: LoadingService,
+    private messagesService: MessagesService) {
   }
 
   ngOnInit(): void {
     this.loadCourses();
-
   }
 
   loadCourses(): void {
@@ -33,10 +34,16 @@ export class HomeComponent implements OnInit {
 
     const courses$ = this.courseService.loadAllCourses()
       .pipe(
-        map(courses => courses.sort(sortCoursesBySeqNo))
+        map(courses => courses.sort(sortCoursesBySeqNo)),
+        catchError(err => {
+          const message = 'Could not load courses';
+          this.messagesService.showErrors(message);
+          console.log(message, err);
+          return throwError(err);
+        })
       );
 
-    const loadCourses$ =  this.loadingService.showLoaderUntilCompleted(courses$);
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
 
     this.beginnerCourses$ = loadCourses$
       .pipe(
